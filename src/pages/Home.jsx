85bd4ed716d7cf13b14941c129f5ce170e804c8f
ozayo <ozay.ozdemir@gmail.com>
2024-09-25 06:16:48 +0200

    og image update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,23 +2,26 @@ import { Helmet } from 'react-helmet-async';
 import MainHero from '../components/MainHero';
 import Features from '../components/Features';
 import Cta from '../components/Cta';
-import homeOgimg from "../assets/chas-seo-og.png"
 
 const Home = () => {
   return (
-    <div className="">
-      <Helmet>
+    <>
+       <Helmet>
         <title>Home - Chas SEO</title>
         <meta name="description" content="Chas Seo website home page." />
         <meta property="og:title" content="Home - Chas SEO" />
         <meta property="og:description" content="Chas Seo website home page." />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content={homeOgimg} />
+        <meta property="og:image" content="https://chas-seo.vercel.app/chas-seo-og.png" />
+        <meta property="og:url" content="https://chas-seo.vercel.app/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Language" content="en" />
+        <link rel="canonical" href="https://chas-seo.vercel.app/" />
       </Helmet>
       <MainHero/>
       <Features />
        <Cta />
-    </div>
+    </>
   );
 }
 
