9e1adbd20bb9495b0596c8a6380e7fe2cc2bec3a
ozayo <ozay.ozdemir@gmail.com>
2024-09-30 01:16:48 +0200

    activate GTM again

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import ReactGA from 'react-ga4'; 
-// import TagManager from 'react-gtm-module';
+import TagManager from 'react-gtm-module';
 
 // Google Analytics'i başlatıyoruz (GA4 Measurement ID'nizi buraya ekleyin)
 ReactGA.initialize("G-M7HQ0KT3WR");
 
 // // GTM Kapsayıcısını başlat
-// const tagManagerArgs = {
-//   gtmId: 'GTM-W2Q5J8PL', // Google Tag Manager ID'nizi buraya ekleyin
-// };
-// TagManager.initialize(tagManagerArgs);
+const tagManagerArgs = {
+  gtmId: 'GTM-W2Q5J8PL', // Google Tag Manager ID'nizi buraya ekleyin
+};
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
