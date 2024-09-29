5ea3bfa9ea5e2bf095f1dc1a065a77ec6f7968ef
ozayo <ozay.ozdemir@gmail.com>
2024-09-30 01:37:26 +0200

    Add GTM pageview in the page

--- src/components/TrackPageView.jsx
+++ src/components/TrackPageView.jsx
@@ -1,16 +1,22 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
+import TagManager from 'react-gtm-module';
 
 const TrackPageView = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Sayfa yüklendiğinde pageview olayını gönderiyoruz
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname,
-      title: document.title
+    // GA4 için pageview gönderiyoruz
+    ReactGA.send({ hitType: "pageview", page: location.pathname, title: document.title });
+
+    // GTM için pageview event'i gönderiyoruz
+    TagManager.dataLayer({
+      dataLayer: {
+        event: 'pageview',
+        page: location.pathname,
+        title: document.title,
+      },
     });
   }, [location]);
 
